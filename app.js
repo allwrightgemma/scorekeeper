10c2b6675e98e691a077579df8b4fa2319089612
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2022-11-03 13:03:40 +0000

    Add max number of points playing to

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ const player2Button = document.querySelector("#player-two-button");
 const resetButton = document.querySelector("#reset-button");
 const player1Score = document.querySelector("#player-one-score");
 const player2Score = document.querySelector("#player-two-score");
+const playingNumber = document.querySelector("#playing-number");
 let player1points = 0;
 let player2points = 0;
 
 player1Button.addEventListener("click", function () {
-  player1points++;
-  player1Score.innerText = player1points;
+  if (player1points < playingNumber.value) {
+    player1points++;
+    player1Score.innerText = player1points;
+  }
 });
+
 player2Button.addEventListener("click", function () {
-  player2points++;
-  player2Score.innerText = player2points;
+  if (player2points < playingNumber.value) {
+    player2points++;
+    player2Score.innerText = player2points;
+  }
 });
 
 resetButton.addEventListener("click", function () {
