0fd73f93bca8e8f64a8676cab59da3934e1591fa
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2022-11-03 15:19:13 +0000

    Added game over to stop play and event listener for playing to

--- app.js
+++ app.js
@@ -3,27 +3,43 @@ const player2Button = document.querySelector("#player-two-button");
 const resetButton = document.querySelector("#reset-button");
 const player1Score = document.querySelector("#player-one-score");
 const player2Score = document.querySelector("#player-two-score");
-const playingNumber = document.querySelector("#playing-number");
+const winningScoreSelect = document.querySelector("#playing-number");
 let player1points = 0;
 let player2points = 0;
+let winningScore = 5;
+let isGameOver = false;
 
 player1Button.addEventListener("click", function () {
-  if (player1points < playingNumber.value) {
+  if (!isGameOver) {
     player1points++;
+    if (player1points === winningScore) {
+      isGameOver = true;
+    }
     player1Score.innerText = player1points;
   }
 });
 
 player2Button.addEventListener("click", function () {
-  if (player2points < playingNumber.value) {
+  if (!isGameOver) {
     player2points++;
+    if (player2points === winningScore) {
+      isGameOver = true;
+    }
     player2Score.innerText = player2points;
   }
 });
 
-resetButton.addEventListener("click", function () {
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
+
+resetButton.addEventListener("click", reset);
+
+function reset() {
+  isGameOver = false;
   player1points = 0;
   player2points = 0;
   player1Score.innerText = 0;
   player2Score.innerText = 0;
-});
+}
